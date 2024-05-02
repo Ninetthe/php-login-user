ae19e891291a5b837237d305452077eaf42b9f74
NINETTHE <raissa.escobar1408@gmail.com>
2024-05-02 17:39:12 +0200

    form login

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&family=Montserrat:ital,wght@0,400;0,700;0,900;1,400&family=Poppins&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css//style.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <input type="text" name="email" placeholder="Enter your mail">
+        <input type="password" name="password" placeholder="Enter your password">
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
